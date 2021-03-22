4094206b9036f90f220343ea481b98ab5acacefb
Bruce Gordon <brc.gordon@gmail.com>
2021-03-22 13:29:39 -0700

    Add courses

---
+++ data/courses.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { courseId: 1001, name: 'Psych 101', time: '8:00 - 9:00 a.m.' },
+  { courseId: 2002, name: 'Science Fiction Cinema', time: '9:00 - 10:00 a.m.' },
+  { courseId: 3003, name: 'Underwater Basket Weaving', time: '10:00 - 11:00 a.m.' },
+  { courseId: 4004, name: 'Greek Comedies', time: '12:00 - 1:00 p.m.' },
+  { courseId: 5005, name: 'Horticulture', time: '1:00 - 2:00 p.m.' },
+  { courseId: 6006, name: 'Fermentation Science', time: '2:00 - 3:00 p.m.' },
+  { courseId: 7007, name: 'Bowling', time: '3:00 - 4:00 p.m.' },
+  { courseId: 8008, name: 'Geology 2', time: '4:00 - 5:00 p.m.' }
+];
