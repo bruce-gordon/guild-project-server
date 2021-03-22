bf069da1e62c3580f4044b577f0ffa1abd4709f5
Bruce Gordon <brc.gordon@gmail.com>
2021-03-22 13:16:52 -0700

    Add users

---
+++ data/users.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 111,
+    name: "Kayla",
+    email: "kayla@sanjunipero.edu"
+  },
+  {
+    id: 112,
+    name: "Alejandra",
+    email: "alejandra@sanjunipero.edu"
+  },
+  {
+    id: 113,
+    name: "Kurt",
+    email: "kurt@sanjunipero.edu"
+  },
+  {
+    id: 114,
+    name: "Scott",
+    email: "scott@sanjunipero.edu"
+  },
+  {
+    id: 115,
+    name: "Montse",
+    email: "montse@sanjunipero.edu"
+  },
+  {
+    id: 116,
+    name: "Jim",
+    email: "jim@sanjunipero.edu"
+  }
+]
