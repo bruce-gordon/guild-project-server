a643bceb1760c0b29bea5d14c3c5adbc25c5bd5a
Bruce Gordon <brc.gordon@gmail.com>
2021-03-22 13:29:54 -0700

    Add enrollments

---
+++ data/enrollments.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    enrollmentId: 1234,
+    courseId: 5005,
+    userId: 111
+  },
+  {
+    enrollmentId: 1555,
+    courseId: 2002,
+    userId: 111
+  },
+  {
+    enrollmentId: 1432,
+    courseId: 5005,
+    userId: 112
+  },
+  {
+    enrollmentId: 1212,
+    courseId: 8008,
+    userId: 113
+  },
+  {
+    enrollmentId: 1333,
+    courseId: 7007,
+    userId: 114
+  },
+  {
+    enrollmentId: 1447,
+    courseId: 4004,
+    userId: 115
+  },
+]
