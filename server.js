88374660babc735da7421f2d44a67503a13cefae
Bruce Gordon <brc.gordon@gmail.com>
2021-03-22 14:20:43 -0700

    Add POST and DELETE endpoints

--- server.js
+++ server.js
@@ -21,3 +21,55 @@ app.get('/courses', (request, response) => {
   const courses = app.locals.courses;
   response.json({ courses })
 })
+
+//GET student by id
+app.get('/users/:id', (request, response) => {
+  const { id } = request.params;
+  const user = app.locals.users.find(user => user.id.toString() === id);
+  if (!user) {
+    return response.status(404).json({
+      message: "Cannot find user with that ID."
+    });
+  }
+  response.status(200).json(user)
+});
+
+//GET enrollments by userId
+app.get('/users/enrollments/:id', (request, response) => {
+  const { id } = request.params;
+  const userEnrollments = app.locals.enrollments.filter(enrollment => enrollment.userId.toString() === id);
+
+  response.status(200).json(userEnrollments);
+});
+
+//DELETE an enrollment by enrollmentId
+app.delete('/enrollments/:id', (request, response) => {
+  const { id } = request.params;
+  const enrollmentList = app.locals.enrollments.filter(enrollment => enrollment.enrollmentId.toString() !== id);
+  app.locals.enrollments = enrollmentList;
+  response.status(200).json({ message: `Enrollment with id ${id} has been removed` })
+})
+
+//POST an enrollment by userId and courseId
+app.post('/enrollments', (request, response) => {
+  const id = Date.now();
+  const enrollment = request.body;
+
+  for (let requiredParameter of ['userId', 'courseId']) {
+    if (!enrollment[requiredParameter]) {
+      response
+        .status(422)
+        .send({ error: `Expected format: { userId: <Number>, courseId: <Number> }. You are missing a "${requiredParameter}" property.`})
+    }
+  }
+
+  const { userId, courseId } = enrollment;
+  if (userId && courseId) {
+    app.locals.enrollments.push({ id, courseId, userId });
+    response.status(201).json({ id, courseId, userId });
+  }
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`)
+})
